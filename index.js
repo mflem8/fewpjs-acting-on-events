fa62cb0d99961d014cb30c2cac79e1de576a9b57
Matt Fleming <36967171+mflem8@users.noreply.github.com>
2021-06-17 18:48:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Your code here
+let dodger = document.getElementById("dodger"); 
 
 function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
@@ -7,12 +7,22 @@ function moveDodgerLeft() {
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
     }
-  } 
+  };
 
   function moveDodgerRight() {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10)
-    if (left > 0) {
-        dodger.style.left = `${left + 1}px`
+      let rightNumbers = dodger.style.left.replace("px", "");
+      let right = parseInt(rightNumbers, 10);
+      dodger.style.left = `${right + 1}px`;
+  };
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
     }
-}
+  });
+
+  document.addEventListener("keydown", function(e) {
+      if (e.key === "ArrowRight") {
+          moveDodgerRight();
+      }
+  });
